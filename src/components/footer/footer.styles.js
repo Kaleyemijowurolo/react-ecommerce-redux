8ac6fe27c68ed8fcfcf411d4835927f39063fb87
Kayode Kolade Caress <97742961+Kaleyemijowurolo@users.noreply.github.com>
2022-11-18 23:31:23 +0000

    added button component and footer dir

---
+++ src/components/footer/footer.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  height: 424px;
+  background: linear-gradient(
+    107.73deg,
+    #2b1e2f 0%,
+    #2a1630 46.88%,
+    #451121 100%
+  );
+`;
