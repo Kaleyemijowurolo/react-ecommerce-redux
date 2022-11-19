3b2fcf3d05fb3cb012445e7d38fff565ccb59c1d
Kayode Kolade Caress <97742961+Kaleyemijowurolo@users.noreply.github.com>
2022-11-19 12:32:50 +0000

    completed footer component

--- src/components/footer/footer.styles.js
+++ src/components/footer/footer.styles.js
@@ -1,13 +1,85 @@
 import styled from "styled-components";
+import { colors } from "../../lib/colors";
 
-export const FooterWraper = styled.footer`
+export const FooterWraper = styled.section`
+  width: 100%;
   height: 424px;
   display: flex;
-  width: 100%;
+  align-items: center;
+  justify-content: center;
   background: linear-gradient(
     107.73deg,
     #2b1e2f 0%,
     #2a1630 46.88%,
     #451121 100%
   );
+`;
+export const FooterDiv = styled.div`
+  width: 85%;
+  /* height: 80%; */
+  display: flex;
+  padding: 1rem 0;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const Footer1 = styled.footer`
+  width: 100%;
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Footer2 = styled.footer`
+  width: 100%;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #e0e0e0;
+
+  .copyright {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 19px;
+    font-style: normal;
+    font-family: "Manrope";
+  }
+
+  .social {
+    display: flex;
+    font-weight: 700;
+    align-items: center;
+    justify-content: space-between;
+
+    svg {
+      margin: 0 5px;
+    }
+  }
+`;
+export const Ul = styled.ul`
+  text-align: left;
+  max-width: 250px;
+  padding: 0;
+  /* margin: 0; */
+  div {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 27px;
+    font-style: normal;
+    color: ${colors.white};
+    font-family: "Manrope";
+    padding-bottom: 10px;
+  }
+  li {
+    font-size: 14px;
+    list-style: none;
+    font-weight: 500;
+    line-height: 30px;
+    font-style: normal;
+    font-family: "Manrope";
+    color: ${colors.white};
+  }
+  .btn {
+    /* margin: 2px 0; */
+  }
 `;
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,13 +1,89 @@
 import React from "react";
 import Button from "../button";
-import { FooterWraper } from "./footer.styles";
+import HorizontalLine from "../divider";
+import Input from "../input";
+import { Ul, Footer1, Footer2, FooterDiv, FooterWraper } from "./footer.styles";
+import { FB, Linkedin, Instagram } from "./icons";
+
+const footerDetails = [
+  {
+    title: "Shop",
+    list: [
+      "Computers",
+      "Phones & tablets",
+      "Gaming & VR",
+      "Audio & Music",
+      "Wearables",
+    ],
+  },
+  {
+    title: "black",
+    list: [
+      "Wearables",
+      "Home entertainment",
+      " E-mobility",
+      "Special products",
+      "All categories",
+    ],
+  },
+  {
+    title: "LL",
+    list: ["About us", "How it works", "Careers", "Investors", "LL care"],
+  },
+  {
+    title: "Help",
+    list: [
+      "+971 5857 05790",
+      "help@ll.com",
+      "FAQ’s",
+      "Returns policy",
+      " Contact us",
+    ],
+  },
+];
 
 const Footer = () => {
   return (
     <FooterWraper>
-      <Button buttonText="Check Now" />
+      <FooterDiv>
+        <Footer1>
+          {footerDetails.map((item, idx) => (
+            <Ul>
+              <div>{item.title}</div>
+              {item.list.map((list) => (
+                <li>{list}</li>
+              ))}
+            </Ul>
+          ))}
 
-      <Button secondary buttonText="Subscribe" />
+          <Ul>
+            <div>Be in the know</div>
+            <li style={{ lineHeight: "15px", margin: "15px 0" }}>
+              Get the latest products, promotions, and design tips in your
+              inbox.{" "}
+            </li>
+
+            <div className="btn">
+              <Input fullwitdth placeholder={"Email address"} />
+            </div>
+            <div className="">
+              <Button fullwitdth buttonText="Subscribe" />
+              {/* <Button fullwitdth secondary buttonText="Subscribe" /> */}
+            </div>
+          </Ul>
+        </Footer1>
+        <HorizontalLine style={{ opacity: 0.1 }} color={"#fff"} />
+
+        <Footer2>
+          <span>© 2022-2022 LL | Privacy · Terms · Sitemap</span>
+          <div className="social">
+            <span>Follow us on</span>
+            <FB />
+            <Linkedin />
+            <Instagram />
+          </div>
+        </Footer2>
+      </FooterDiv>
     </FooterWraper>
   );
 };
